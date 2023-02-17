38ab9874d5de04ea7f0ab5e79e9627c20737ead6
Lucas Karlsson <lucas.karlsson@post.com>
2023-02-17 10:42:01 +0100

    added retrieveOrder

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-
-import {config} from "dotenv";
+import { config } from "dotenv";
 import express from "express";
 config();
-import { createOrder } from "./klarna.js";
+import { createOrder, retrieveOrder } from "./klarna.js";
 
 const PORT = process.env.PORT;
 
 const products = [
-    {id:"1",name:"Chair",price:199},
-    {id:"2",name:"Table",price:499},
-    {id:"3",name:"Lamp",price:299}
+  { id: "1", name: "Chair", price: 199 },
+  { id: "2", name: "Table", price: 499 },
+  { id: "3", name: "Lamp", price: 299 },
 ];
 const app = express();
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send(products.map(p=>{
-        return `<a href="/p/${p.id}">${p.name}</a><br>`
-    }).join(""));
+app.get("/", (req, res) => {
+  res.send(
+    products
+      .map((p) => {
+        return `<a href="/p/${p.id}">${p.name}</a><br>`;
+      })
+      .join("")
+  );
 });
 
-app.get("/p/:id",async(req,res)=>{
-    const product = products.find(p=>p.id===req.params.id);
-    const data = await createOrder(product);
-    res.send(data.html_snippet);
+app.get("/p/:id", async (req, res) => {
+  const product = products.find((p) => p.id === req.params.id);
+  const data = await createOrder(product);
+  res.send(data.html_snippet);
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server started on port: ${PORT}`);
+app.get("/confirmation", async (req, res) => {
+  const data = await retrieveOrder(req.query.order_id);
+  res.send(data.html_snippet);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
 });
